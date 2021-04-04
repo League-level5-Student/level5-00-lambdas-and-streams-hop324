b1fcf8ac681899e897f659ab05163e8e3e9c6120
hop324 <39838785+hop324@users.noreply.github.com>
2021-04-04 16:25:40 -0700

    Lambda Buttons (IP)
    :+1: :100: :sunglasses: Just spent the entire time researching lambda expressions

--- src/_00_JButtons_with_Lambdas/LambdaButtons.java
+++ src/_00_JButtons_with_Lambdas/LambdaButtons.java
@@ -1,11 +1,13 @@
 package _00_JButtons_with_Lambdas;
 
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
-public class LambdaButtons {
+public class LambdaButtons implements ActionListener{
 	private JFrame window = new JFrame();
 	private JButton addNumbers = new JButton("ADD 2 Numbers");
 	private JButton randNumber = new JButton("RANDOM NUMBER");
@@ -15,8 +17,11 @@ public class LambdaButtons {
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setLayout(new FlowLayout());
 		window.add(addNumbers);
+		addNumbers.addActionListener(this);
 		window.add(randNumber);
+		randNumber.addActionListener(this);
 		window.add(tellAJoke);
+		tellAJoke.addActionListener(this);
 		
 		//1. Call the addActionListener methods for each button. Use lambdas
 		//   to define to functionality of the buttons.
@@ -27,6 +32,12 @@ public class LambdaButtons {
 	
 	public static void main(String[] args) {
 		new LambdaButtons();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 	
 	
